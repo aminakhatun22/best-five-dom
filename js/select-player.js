1fcb60f19270966956f548b87378596ca4851ce1
aminakhatun22 <aminak26802@gmail.com>
2022-08-22 03:47:36 +0600

    added third step

--- js/select-player.js
+++ js/select-player.js
@@ -1,1 +1,19 @@
-console.log('fdkj')
+
+
+
+document.getElementById('select-1').addEventListener('click', function () {
+
+    const playerName = document.getElementById('player-one');
+    const name = playerName.innerText;
+
+    const setName = document.getElementById('ul-list');
+    setName.innerText = name;
+
+
+
+
+});
+
+
+
+
